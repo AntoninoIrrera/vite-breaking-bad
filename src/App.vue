e65890745d90c1f1a70ebe94df35976a47abae47
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-01-11 15:37:00 +0100

    inserimento dinamico di componente card tramite richiamo di API

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,64 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script >
+import {store} from './store';
+import axios from 'axios';
+
+import AppCard from './components/AppCard.vue'
+
+export default{
+  components:{
+    AppCard,
+  },
+  data(){
+    return{
+      store,
+    }
+  },
+  methods:{
+    getCardFromAPI(){
+      axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=12&offset=0')
+        .then((response) => {
+          console.log(response.data.data);
+
+          store.cardList = response.data.data;
+        })
+        
+    }
+  },
+  created(){
+    this.getCardFromAPI();
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h2>trovate {{ store.cardList.length }} carte</h2>
+  <main class="vetrinaCard">
+    <div class="singleCard" v-for="card in store.cardList">
+      <AppCard :nameCard="card.name" :typeCard="card.type" :imgCard="card.card_images[0].image_url" />
+    </div>
+  </main>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+.vetrinaCard{
+    display: flex;
+    flex-wrap: wrap;
+
+    padding: 5rem;
+
+  }
+
+  .singleCard{
+    width: calc(100% / 3 - 10px);
+
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  h2{
+    margin-top: 2rem;
+    text-align: center;
+  }
 </style>
---
+++ src/components/AppCard.vue
@@ -0,0 +1,34 @@
+<script >
+import { store } from '../store.js'
+
+export default {
+   data(){
+    return{
+        store,
+    }
+   },
+   props:{
+       nameCard: String,
+       typeCard: String,
+       imgCard: null,
+   }
+}
+</script>
+
+<template>
+
+    <img :src="imgCard" :alt="nameCard">
+    <h3>{{ nameCard }}</h3>
+    <h3>{{ typeCard }}</h3>
+
+
+
+</template>
+
+<style scoped>
+    img{
+        width: 50%;
+    }
+
+   
+</style>
