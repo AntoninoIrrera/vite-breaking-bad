c676c0774acfd79a0bf8a0c0f1c4e3b68380ebfc
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-01-11 15:57:17 +0100

    aggiunto loader caricamento card

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
 import {store} from './store';
 import axios from 'axios';
 
-import AppCard from './components/AppCard.vue'
+import AppCard from './components/AppCard.vue';
+import AppLoader from "./components/AppLoader.vue";
 
 export default{
   components:{
     AppCard,
+    AppLoader,
   },
   data(){
     return{
@@ -33,16 +35,22 @@ export default{
 <template>
   <h2>trovate {{ store.cardList.length }} carte</h2>
   <main class="vetrinaCard">
-    <div class="singleCard" v-for="card in store.cardList">
+    <div v-if="store.cardList.length == 0">
+      <AppLoader/>
+    </div>
+    <div class="singleCard" v-for="card in store.cardList" v-else>
       <AppCard :nameCard="card.name" :typeCard="card.type" :imgCard="card.card_images[0].image_url" />
     </div>
   </main>
 </template>
 
 <style scoped>
+
 .vetrinaCard{
     display: flex;
     flex-wrap: wrap;
+
+    justify-content: center;
 
     padding: 3rem;
 
